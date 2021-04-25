b4d11b58ed66852f19b224c8ad268d2516527bf1
kevAs <kevassiobo@gmail.com>
2021-04-25 11:58:16 +0200

    base

---
+++ main.js
@@ -0,0 +1,62 @@
+//1
+// console.log(1 == "1");
+//2
+// console.log(1 === "1");
+//3
+// let x = prompt('votre prenom');
+// if(x.length <5 ){
+//     alert('Ton nom est trop court ')
+// }
+//4
+// let x = 5;
+// let y = 4;
+// let z = x + y;
+// alert(x + ' + ' + y );
+// let u = prompt('la solution est  ?');
+// if(z == u ){
+//     alert('Félicitaion')
+// }
+// else{
+//     alert("c' est incorrect")
+// }
+//5
+let x = 5;
+let y = 5;
+let z = x * y;
+alert(x + ' * ' + y );
+let u = prompt('la solution est  ?');
+if(z == u){
+    alert('Feliciation la réponse est bien '+ u)
+}
+else{
+    let k = z-u;
+    alert("C'est dommage, tu n'étais qu'a " +  k +" unité de la bonne réponse")
+}
+//6
+// let x = [];
+// let z = prompt('entrez des élémnts');
+// let y =prompt('entrez des élémnts');
+// let i = prompt('entrez des élémnts');
+//  x.push(z, y, i);
+//  if(x.length == 3){
+//      alert('tableau');
+//  }
+//7
+// let x = prompt('choisit un chiffre dans un bac');
+// let z = prompt(' entrez le chiffres 2 ');
+// let t = [];
+// let v = [];
+// if ( x >= 12  && z >= 12 ){
+//     t.push(x), t.push(z)
+// }
+// else {
+//    v.push(z), v.psuh(x);
+// }
+// alert(t , v);
+// let x  = prompt('Entrez votre age ')
+// if(x > = 18){
+//     alert(' vous assez mature pour notre contenu');
+// }
+//     else{
+//         alert('acces interdit tu es trop jeune petit');
+//     }
